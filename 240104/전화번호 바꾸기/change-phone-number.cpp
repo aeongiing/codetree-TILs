5dbeda318e23a3130aef48f4c96a2d203fdf4ed2
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-04 19:36:15 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240104/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, xxxx, yyyy;
+    cin>>a;
+    cin.get();
+    cin>>xxxx;
+    cin.get();
+    cin>>yyyy;
+    cout<<"010-"<<yyyy<<"-"<<xxxx;
+    return 0;
+}
