f5c7ca62bd6615608f358e6162b4763d3522267d
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-06 01:03:25 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s, a;
+    cin >> s >> a;
+
+    if(s==0){
+        if(a>=19){
+            cout << "MAN";
+        }
+        else
+            cout << "BOY";
+    }
+    else if(s==1){
+        if(a>=19){
+            cout << "WOMAN";
+        }
+        else
+            cout <<"GIRL";
+    }
+    return 0;
+}
