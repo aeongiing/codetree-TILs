89556216f00a29fd04e51257e98483fb1d3615bf
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-06 21:59:25 +0900

    Title: 홀수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/홀수만 출력/output-only-odd.cpp
+++ 240106/홀수만 출력/output-only-odd.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    if(a%2==0){
-        for(int i=a+1; i<=b; i+=2){
-            cout << i <<" ";
-        }
+    // if(a%2==0){
+    //     for(int i=a+1; i<=b; i+=2){
+    //         cout << i <<" ";
+    //     }
+    // }
+    // else{
+    for(int i=a; i<=b; i+=2){
+        cout << i <<" ";
     }
-    else{
-        for(int i=a; i<=b; i+=2){
-            cout << i <<" ";
-        }
-    }
+    //}
     return 0;
 }
