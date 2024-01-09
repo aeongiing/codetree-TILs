3354b4b02d24a3440f45dc620a23cad21bfb311d
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-10 02:55:07 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt3=0, cnt5=0;
+    for(int i=0; i<10; i++){
+        int a;
+        cin >> a;
+        if(a%3==0){
+            cnt3++;
+        }
+        if(a%5==0){
+            cnt5++;
+        }
+    }
+    cout << cnt3 << " " << cnt5;
+    return 0;
+}
