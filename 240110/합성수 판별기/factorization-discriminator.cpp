9e8cfe5cc38bd8170bd5d79742bc24350473d405
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-10 20:20:24 +0900

    Title: 합성수 판별기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool tf=false;
+    cin >> n;
+    for(int i=2; i<n; i++){
+        if(n%i!=0){
+            tf=true;
+        }
+    }
+    if(tf==true){
+        cout << "C";
+    }
+    else{
+        cout << "N";
+    }
+    return 0;
+}
