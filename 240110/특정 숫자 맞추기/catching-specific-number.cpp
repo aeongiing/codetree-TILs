de2056b7466c6a78ee6ebc3faec1aa6151fafaf5
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-10 19:40:07 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while(true){
+        int n;
+        cin >> n;
+        if(n<25){
+            cout << "Higher" << endl;
+        }
+        else if(n>25){
+            cout << "Lower" << endl;
+        }
+        else{
+            cout << "Good"<<endl;
+            break;
+        }
+
+    }
+    return 0;
+}
