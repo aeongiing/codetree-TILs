2c597ab5215269a343b27cb7cc48808feec86ac4
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-19 23:21:02 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240119/별표 출력하기 10/print-star-10.cpp
+++ 240119/별표 출력하기 10/print-star-10.cpp
@@ -5,14 +5,14 @@ int main() {
     int n;
     cin >> n;
     for(int i=0; i<2*n; i++){
-        if(i%2!=0){
-            for(int j=0; j<n-(i-1)/2; j++){
+        if(i%2==0){
+            for(int j=0; j<1 + (i / 2); j++){
                 cout << "* ";
             }
             cout << endl;
         }
         else{
-            for(int j=0; j<1+(i/2); j++){
+            for(int j=0; j<n - (i - 1) / 2; j++){
                 cout << "* ";
             }
             cout << endl;
