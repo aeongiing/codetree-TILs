b85666b2f962a85b83b35837af4b0d06797d4b83
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-20 21:39:27 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240120/완전수/perfect-number.cpp
+++ 240120/완전수/perfect-number.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main() {
     int start, end;
     cin >> start >> end;
-    int num=0, cnt=0;
+    int cnt=0;
     for(int i=start; i<=end; i++){
+        int num=0;
         for(int j=1; j<i; j++){
             if(i%j==0){
                 num+=j;
