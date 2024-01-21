98e3a38cdadbf5c7f2d5118ca0ccb0a6bdedfbfe
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-21 16:29:14 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240121/10개 입력받기/receive-10-inputs.cpp
+++ 240121/10개 입력받기/receive-10-inputs.cpp
@@ -8,15 +8,20 @@ int main() {
 
     for(int i=0; i<10; i++){
         cin >> arr[i];
+        cout.precision(1);
         if(arr[i]!=0){
             sum+=arr[i];
             num=i+1;
         }
         else{
             double avg=(double)sum/num;
-            cout.precision(1);
             cout << sum << " " << fixed << avg;
         }
+        if(i==9){
+            double avg=(double)sum/num;
+            cout << sum << " " << fixed << avg;
+        }
+        
     }
     return 0;
 }
