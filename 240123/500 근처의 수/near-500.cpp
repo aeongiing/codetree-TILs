4655e4261eed409d120afd04e810ef57d1cab572
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-23 00:17:52 +0900

    Title: 500 근처의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/500 근처의 수/near-500.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int max=0;
+    int min=1000;
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+        if(arr[i]<500){
+            if(arr[i]>=max){
+                max=arr[i];
+            }
+        }
+        else if(arr[i]>500){
+            if(arr[i]<=min){
+                min=arr[i];
+            }
+        }
+    }
+    cout << max << " " << min;
+    return 0;
+}
