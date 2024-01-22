8aa0ca31c698187668b1a1b73fc7edf94c8a9a31
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-22 23:14:53 +0900

    Title: n개의 숫자 중 최소, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240122/n개의 숫자 중 최소/min-of-n-num.cpp
+++ 240122/n개의 숫자 중 최소/min-of-n-num.cpp
@@ -5,16 +5,17 @@ int main() {
     int n;
     cin >> n;
     int arr[n];
-    int min=INT_MAX;
     int cnt=0;
-    for(int i=0; i<n; i++){
+    cin >> arr[0];
+    int min=arr[0];
+    for(int i=1; i<n; i++){
         cin >> arr[n];
         if(arr[n]<=min){
             min=arr[n];
         }
     }
     for(int i=0; i<n; i++){
-        if(arr[n]==min){
+        if(arr[i]==min){
             cnt++;
         }
     }
