9c56856dbfbae1db1e7024eb3685e83cc78200f4
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-24 23:32:37 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    if(c=='a'){
+        cout << 'z';
+    }
+    else{
+        cout << (char)(c-1);
+    }
+    return 0;
+}
