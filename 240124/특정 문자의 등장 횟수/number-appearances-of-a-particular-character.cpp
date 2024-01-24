46079283dc7a730d71451d1ffabb798beb092f30
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-01-24 22:22:57 +0900

    Title: 특정 문자의 등장 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int cnt1=0, cnt2=0;
+    for(int i=0; i<s.length()-1; i++){
+        if(s[i]=='e' && s[i+1]=='e'){
+            cnt1++;
+        }
+        if(s[i]=='e' && s[i+1]=='b'){
+            cnt2++;
+        }
+    }
+    cout << cnt1 << " " << cnt2;
+    return 0;
+}
