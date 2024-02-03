7e71ab0713093dddd46f39ed9cc0284d4e4c399e
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-02-03 17:16:01 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240203/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool IsPrime(int i){
+    for(int j=2; j<i; j++){
+        if(i%j==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int sum=0;
+    for(int i=a; i<=b; i++){
+        if(IsPrime(i)){
+            sum+=i;
+        }
+    }
+    cout << sum;
+    return 0;
+}
