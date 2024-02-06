92cd5cf0f4d859bd4b9f56c8e5ad9b54ef11af16
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-02-06 16:22:08 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240206/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240206/2개씩 그룹짓기/group-of-pairs.cpp
@@ -5,18 +5,12 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int arr[n][2];
-    for(int i=0; i<n; i++){
-        for(int j=0; j<2; j++){
-            cin >> arr[i][j];
-        }
+    int arr[2*n];
+    for(int i=0; i<2*n; i++){
+        cin >> arr[i];
     }
-    int arr_sum[n];
-    for(int i=0; i<n; i++){
-        arr_sum[i]=arr[i][0]+arr[i][1];
-    }
-    sort(arr_sum, arr_sum+n);
-    cout << arr_sum[n-1];
+    sort(arr, arr+2*n);
+    cout << arr[n]+arr[n-1];
 
 
     return 0;
