e58719ac07a4a281275e810297cdfe6eca3da095
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-02-19 00:51:44 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240219/그 요일은/the-day-of-the-day.cpp
+++ 240219/그 요일은/the-day-of-the-day.cpp
@@ -9,7 +9,23 @@ int main() {
     cin >> day;
 
     int num_of_days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int elapsed_days=1;
+    int day_index;
+    if(day == "Mon")
+        day_index = 0;
+    else if(day == "Tue")
+        day_index = 1;
+    else if(day == "Wed")
+        day_index = 2;
+    else if(day == "Thu")
+        day_index = 3;
+    else if(day == "Fri")
+        day_index = 4;
+    else if(day == "Sat")
+        day_index = 5;
+    else if(day == "Sun")
+        day_index = 6;
+
+    int elapsed_days=0;
     while(true) {
         if(m1 == m2 && d1 == d2)
             break;
@@ -22,13 +38,11 @@ int main() {
             d1 = 1;
         }
     }
-    int dday=elapsed_days%7;
-    if(dday==0){
-        cout << elapsed_days/7;
-    }
-    else{
-        cout << elapsed_days/7+1;
-    }
+    int num_of_target_day = elapsed_days/7;
+    if(elapsed_days%7 >= day_index)
+        num_of_target_day++;
+
+    cout << num_of_target_day;
     
     return 0;
 }
