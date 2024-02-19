a19750e40b45fabe91c3202222d4749df5520073
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-02-19 19:55:53 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240219/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240219/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -8,8 +8,9 @@ int main() {
     int x1, x2;
     for(int i=0; i<n; i++){
         cin >> x1 >> x2;
-        array[x1+100]++;
-        array[x2+100]++;
+        for(int j=x1+100; j<x2+100; j++){
+            array[j]++;
+        }
     }
     int max=0;
     for(int i=0; i<200; i++){
