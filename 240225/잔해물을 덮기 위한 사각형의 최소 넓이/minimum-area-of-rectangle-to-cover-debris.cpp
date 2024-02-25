38b03809cdcf446bc74a0f8145dd327af4020bb9
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-02-25 23:05:06 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 20ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240225/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
+++ 240225/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -2,49 +2,40 @@
 using namespace std;
 
 int main() {
-    int x1[2], y1[2];
-    int x2[2], y2[2];
-    int arr[2001][2001];
-
-    for(int i=0; i<2; i++){
-        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
-
-        x1[i]+=1000;
-        y1[i]+=1000;
-        x2[i]+=1000;
-        y2[i]+=1000;
+    int arr[2001][2001]={};
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    for(int i=x1+1000; i<x2+1000; i++){
+        for(int j=y1+1000; j<y2+1000; j++){
+            arr[i][j]=1;
+        }
     }
-    for(int i=0; i<2; i++){
-        for(int x=x1[i]; x<x2[i]; x++){
-            for(int y=y1[i]; y<y2[i]; y++){
-                arr[x][y]=i+1;
+    cin >> x1 >> y1 >> x2 >> y2;
+    for(int i=x1+1000; i<x2+1000; i++){
+        for(int j=y1+1000; j<y2+1000; j++){
+            arr[i][j]=0;
+        }
+    }
+    for(int i=0; i<2001; i++){
+        for(int j=0; j<2001; j++){
+            if(arr[i][j]==1){
+                x1=i;
+                y1=j;
+                goto point;
             }
         }
     }
-
-    int minX=2000, maxX=0, minY=2000, maxY=0;
-    bool exist=false;
-    for(int x=0; x<=2000; x++){
-        for(int y=0; y<=2000; y++){
-            if(arr[x][y]==1){
-                exist=true;
-                minX=min(minX, x);
-                maxX=max(maxX, x);
-                minY=min(minY, y);
-                maxY=max(maxY, y);
+    point:
+    for(int i=2000; i>=0; i--){
+        for(int j=2000; j>=0; j--){
+            if(arr[i][j]==1){
+                x2=i;
+                y2=j;
+                goto point2;
             }
         }
     }
-
-    int size;
-    if(!exist){
-        size=0;
-    }
-    else{
-        size=(maxX-minX+1)*(maxY-minY+1);
-    }
-    cout << size;
-
-
+    point2:
+    cout << (x2-x1+1)*(y2-y1+1);
     return 0;
 }
