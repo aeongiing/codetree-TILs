546c597ee8a87cb72164cefbf354e1be84037d4a
aeongiing <146530396+aeongiing@users.noreply.github.com>
2024-02-26 17:19:55 +0900

    Title: 연속되는 수 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int max=1;
+    for(int i=1; i<n; i++){
+        int cnt=1;
+        while(arr[i]>arr[i-1] && i<n){
+            cnt++;
+            i++;
+        }
+        if(cnt>max){
+            max=cnt;
+        }
+    }
+    cout << max;
+    return 0;
+}
